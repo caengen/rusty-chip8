22a71f07983c753a95daeeefdfb30c0955e7444a
Christian Engen <chris.a.engen@gmail.com>
2023-02-12 11:10:32 +0100

    add pkgs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("rusty-chip8")]
+async fn main() {
+    request_new_screen_size(64.0, 64.0);
+    loop {}
 }
